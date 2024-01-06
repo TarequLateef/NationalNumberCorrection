9e2d07dde88fd9a384aafbeec8c9687f3e4468c3
Tarequ Lateef <146071062+TarequLateef@users.noreply.github.com>
2024-01-06 12:56:00 +0200

    replace files
    add classes in app module and app componetn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
+import { NationalComponent } from './Comp/national/national.component';
+import { NumberinputComponent } from './Comp/NumberInput/numberinput.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NationalComponent,
+    NumberinputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
